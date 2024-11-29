916f968c8ca8ed6997b7098fc5b3c5c4eb5e7d59
ryosantouchh <kittitat.suntimak@gmail.com>
2024-11-29 11:59:46 +0700

    feat: add module to app

--- data-anonymous-backend/src/app.module.ts
+++ data-anonymous-backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { PinoLoggerModule } from './common/logger/logger.module';
 import { ExceptionModule } from './common/exception/exception.module';
 import { DatabaseModule } from './common/database/database.module';
 import { UserModule } from './domain/user/user.module';
+import { PostModule } from './domain/post/post.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UserModule } from './domain/user/user.module';
     DatabaseModule,
 
     UserModule,
+    PostModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
